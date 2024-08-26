e953a0b1a91a494a87217ec05ecb4c93a2b9e744
Mike Fritzsche <mikef65@gmail.com>
2024-08-26 16:04:41 -0700

    home animation

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',  // This makes the server accessible from the outside Docker network
+    port: 5173        // Ensure this matches the port Traefik is routing to
+  }
 })
